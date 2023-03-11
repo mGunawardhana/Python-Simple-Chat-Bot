6460483da66bea9a93eff67c6da6b7da76c73e81
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-03-11 20:31:05 +0530

    content update

--- SimpleChatBot.py
+++ SimpleChatBot.py
@@ -41,8 +41,8 @@ def run_alexa():
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I:%M %p')
         talk('Current time is ' + time)
-    elif 'who the heck is ' in command:
-        person = command.replace('who the heck is', '')
+    elif 'what is the ' in command:
+        person = command.replace('what is the', '')
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
